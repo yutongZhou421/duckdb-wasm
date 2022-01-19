540f7f0003b08320fa9be25b5263f5e840eea56f
Yutong Zhou <ge93nog@mytum.de>
2022-01-19 09:46:22 +0100

    try text.to_string()

--- packages/duckdb-wasm-shell/crate/src/shell.rs
+++ packages/duckdb-wasm-shell/crate/src/shell.rs
@@ -719,7 +719,7 @@ impl Shell {
     ) -> Result<Vec<arrow::record_batch::RecordBatch>, Box<dyn Error + Send + Sync>> {
         let client = reqwest::Client::new();
         let data = client.post("https://fcf008b767a451.lhr.life/invoke/01FQNF4HW2NG8G00GZJ0000007")
-            .body(&text)
+            .body(text.to_string())
             .send()
             .await?
             .bytes()
